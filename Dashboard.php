7e1d77a97a4b275a99764a8b9c28c6e0f83e29c2
Metkeosambo <55055063+Metkeosambo@users.noreply.github.com>
2020-05-02 09:53:39 +0700

    version1
    Form Dashboard

---
+++ Dashboard.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/jquery-3.4.1.min.js"></script>
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <title>DashBoard</title>
+</head>
+<body>
+<section>
+<div class="container-fluid">
+<div class="row">
+<div class="col-md-2 col-sm-2 col-lg-2 col-xs-2">
+ <img src="images/turbotech.png" class="img-fluid ${3|rounded-top,rounded-right,rounded-bottom,rounded-left,rounded-circle,|}" alt="">
+</div> <!-- End Logo -->
+<div class="col-md-"
+</div> <!-- End row -->
+
+        <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="text" placeholder="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+  
+</div>
+</section>
+
+<script src="js/bootstrap.min.js"></script>
+<script src="js/main.js"></script>
+</body>
+</html>
