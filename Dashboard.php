36a3a6e2a41ff1afcedba2b703fbd9df419d19b1
Metkeosambo <55055063+Metkeosambo@users.noreply.github.com>
2020-05-08 14:42:54 +0700

    update
    update

--- Dashboard.php
+++ Dashboard.php
@@ -2,6 +2,7 @@
 <?php
 include "connect.php";
 session_start();
+$r=session_id();
 	?>
 </script>
 <!DOCTYPE html>
@@ -11,31 +12,36 @@ session_start();
 <section class="menu">
 <?php include ('leftsidemenu.php') ?>
     <!-- MAIN Contain -->
-<!-- MAIN Contain -->
-<!-- MAIN Contain -->
+     <!-- MAIN Contain -->
+      <!-- MAIN Contain -->
     <div class="col-lg-9 col-md-9 col-xs-12  col-sm-12" id="main-con">
         <div class="row">
              <?php
-               $stmt1 = $con->prepare("select id from menu where title='$_SESSION[role]' AND status=1");
-               $stmt1->execute();
-               while($row=$stmt1->fetch()){
-                $id = $row['id'];
-               }
-              $stmt = $con->prepare("select * from content where id_menu='$id' AND status=1");
-              $stmt->execute();
-              while($row1=$stmt->fetch()){
-                $link = exec("$row1[link]");
-              echo "
+             
               
-              <div class='col-lg-3 col-md-3 col-xs-4  col-sm-4'>
-              <div class='middle'>
-              ";
-	          echo "<a class='btn-edit' target='_blank' href='$link'> 
-              <i class='$row1[icon]'></i>	
-     </a>";
-              echo "</div>
-                    </div>";
-                               
+              
+                $stmt1 = $con->prepare("select id from menu where title='$_SESSION[role]' AND status=1");
+                $stmt1->execute();
+                while($row=$stmt1->fetch()){
+                 $id = $row['id'];
+                }
+               $stmt = $con->prepare("select * from content where id_menu='$id' AND status=1");
+               $stmt->execute();
+               while($row1=$stmt->fetch()){
+                
+                if (session_id()==$r)
+                {
+                  $link = $row1['link'];
+                }else{$link = exec("$row1[link]");}
+               echo " 
+               <div class='col-lg-3 col-md-3 col-xs-4  col-sm-4'>
+               <div class='middle'>
+               ";
+             echo "<a class='btn-edit' target='_blank' href='$link'> 
+               <i class='$row1[icon]'></i>	
+      </a>";
+               echo "</div>
+                     </div>";
     }
         ?>
 </div><!-- Main Row END -->
@@ -57,5 +63,6 @@ session_start();
 <script src="js/bootstrap.min.js"></script>
 <script src="js/main.js"></script>
 <script src="js/all.js"></script>
+
 </body>
 </html>
